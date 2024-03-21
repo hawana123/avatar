933cea15dcfa32defa8db0d6419253c5e73802be
niliang <niliang@micvs.com>
2024-03-21 11:41:27 +0800

    haaa

--- src/pages/components/Modal/Embed/index.tsx
+++ src/pages/components/Modal/Embed/index.tsx
@@ -30,7 +30,7 @@ export default function EmbedModal({
 
   useEffect(() => {
     setUrl(
-      `${process.env.NEXT_PUBLIC_URL}/api/${apiType[imageType]}/${btoa(
+      `https://av.zoz.la/api/${apiType[imageType]}/${btoa(
         JSON.stringify(config),
       )}`,
     );
